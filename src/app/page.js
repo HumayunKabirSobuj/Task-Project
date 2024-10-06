bf987aab15d2e4876c036b3725d1eb6a26b0db66
HumayunKabirSobuj <humayunkabirgithub@gmail.com>
2024-10-07 03:35:34 +0600

    modify

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Cards from "@/Components/Cards";
-import Heading from "@/Components/Heading";
 import Navbar from "@/Components/Navbar";
 import Table from "@/Components/Table";
 
@@ -9,7 +8,6 @@ export default function Home() {
       <Navbar></Navbar>
       <Cards></Cards>
       <Table></Table>
-      <Heading></Heading>
     </div>
   );
 }
